3b15816f33bac7bb80763c4994316c1f0a407909
Jardel Bordignon <jardel1101@gmail.com>
2022-09-09 20:23:58 -0300

    feat: absolute imports src

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+/* eslint-disable semi */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json', 'cjs'],
+        alias: {
+          src: './src',
+        },
+      },
+    ],
+  ],
+}
