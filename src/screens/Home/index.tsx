acca0f07d48ebd906b2f3cfbc7c01cbd6c894d9c
Jardel Bordignon <jardel1101@gmail.com>
2022-09-10 01:50:09 -0300

    feat: react-native-vector-icons config

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import * as S from './styles'
 
 export function Home() {
   return (
     <S.Wrapper>
+      <Icon name="home" size={24} />
       <S.Title>Title</S.Title>
       <S.Subtitle>Subtitle</S.Subtitle>
       <S.Paragraph>Paragraph</S.Paragraph>
